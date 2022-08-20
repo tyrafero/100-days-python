3621f67e4c583f0ca2ac780da016267929d3f89a
tyrafero <lehanhunter@gmail.com>
2022-08-20 15:11:42 +0545

    day2

---
+++ day2.py
@@ -0,0 +1,12 @@
+print("Welcome to the tip calculator \n")
+totalBill= input("What was the total bill? \n")
+noPeople= input("Enter number of people \n")
+
+percentageTip= input("Percent of tip you like to give, 10|20|30 \n")
+
+divideEach= float(totalBill)/int(noPeople)
+
+totalPay= float(divideEach) + float(divideEach*(float(percentageTip)/100))
+
+print("Each one of " +noPeople +" of you should pay \n")
+print(totalPay)
