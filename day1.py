48f6f2fadc799d91834b11d038f91e344484733f
tyrafero <lehanhunter@gmail.com>
2022-08-20 14:42:20 +0545

    day1

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Program to generate band name using city the user was born and the name of the user's pet
+
+print("Hey fellow")
+
+user_city= input("Where were you born \n")
+user_pet= input("What is the name of the pet \n")
+
+band_name= user_city + user_pet
+print(band_name)
+
