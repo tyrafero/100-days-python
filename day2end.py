793a6ddaf41ee1afbdfff7cfa6a25fddb5315dde
tyrafero <lehanhunter@gmail.com>
2022-08-20 18:42:56 +0545

    day two end

--- day2end.py
+++ day2end.py
@@ -4,3 +4,14 @@ name= "Tyra"
 place= "California"
 isAvailable=True
 print(f"My name is {name}, I live in {place},  My availability is {isAvailable}")
+
+
+age= input("Please enter your current age \t")
+
+leftYears= 90- int(age)
+
+days= leftYears*365
+months= leftYears*12
+weeks= leftYears*52
+
+print(f"You are {age} years now and you have {days} days, {months} months and {weeks} weeks to live.")
