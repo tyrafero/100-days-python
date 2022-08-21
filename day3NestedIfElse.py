b36338df6ef80d098f05f56d41f4d6a082fee9fd
tyrafero <lehanhunter@gmail.com>
2022-08-21 17:39:30 +0545

    redone

--- day3NestedIfElse.py
+++ day3NestedIfElse.py
@@ -1,12 +1,17 @@
+print("Welcome to World of Rollers")
+print("_____________________________")
+
 height= int(input("Enter your height: "))
-age= int(input("Enter your age: "))
+
 
 if height > 120:
     print("You can ride the roller coaster.")
-    
-    if age > 18:
-        print("Pay $12 at the counter")
-    else:
+    age= int(input("Enter your age: "))
+    if age <= 18 and age >=12:
         print("Pay $7 at the counter")
+    elif age <12:
+        print("Pay $5 at the counter")
+    else:
+        print("Pay $12 at the counter")
 else:
     print("Sorry, you can't ride the roller.")
