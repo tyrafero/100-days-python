cc3b78f920e13de39e45faf00ed8a03da32da8ec
tyrafero <lehanhunter@gmail.com>
2022-08-21 17:40:51 +0545

    updated

--- day2BmiCalculator.py
+++ day2BmiCalculator.py
@@ -1,6 +1,8 @@
+print("Welcome to your own BMI Calculator.")
+print("____________________________________")
+
 weight= input("Please enter your weight in Kg \n")
 height = input("Please enter your height in Cm \n")
 
 bmi= (float(weight)/(float(height)**2))*10000   #converting cm to metre
-
 print("Your BMI is " + str(bmi))
